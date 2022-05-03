cb6f8dd2b9b379993eddf0dcd4e5a7a71a5a4371
andrea.giulianelli <andrygiulio@gmail.com>
2022-05-03 15:21:14 +0200

    ex 3

---
+++ src/test/scala/testLab/TestShopping.scala
@@ -0,0 +1,37 @@
+package testLab
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class TestCart extends AnyFunSuite:
+  test("A cart should be initially empty") {
+    val cart = BasicCart()
+    assert(cart.size == 0)
+  }
+
+class TestCatalog extends AnyFlatSpec with Matchers:
+  "Two piece of a 10 dollar product" should "cost 20 dollars" in {
+    BasicCatalog(Map(Product("TestProduct") -> Price(10))).priceFor(Product("TestProduct"), 2) shouldBe Price(20)
+  }
+
+class Fixture:
+  val warehouse = BasicWarehouse()
+
+def fixture = new Fixture
+
+class TestWarehouse extends AnyFreeSpec with Matchers:
+  "A Warehouse" - {
+    val f = fixture
+    "when empy" - {
+      "should have 0 quantity for any given product" in {
+        f.warehouse.get(Product("TestProduct"), 1) shouldBe (Product("TestProduct"), 0)
+      }
+    }
+  }
+
+
+
+
+
