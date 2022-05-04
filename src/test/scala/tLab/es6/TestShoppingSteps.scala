a13e595bb6a4a397c14bfcf199d2ca73ad084320
andrea.giulianelli <andrygiulio@gmail.com>
2022-05-04 09:50:59 +0200

    error#2

--- src/test/scala/tLab/es6/TestShoppingSteps.scala
+++ src/test/scala/tLab/es6/TestShoppingSteps.scala
@@ -7,13 +7,13 @@ import testLab.Price
 class TestShoppingSteps extends ScalaDsl with EN:
     val p = Price(10)
 
-    Given("""A shopping""") { () =>
-
+    Given("""^A shopping$""") {
+        println("ciao")
     }
-    When("""I want to but something""") { () =>
-
+    When("""^I want to but something$""") {
+        println("come")
     }
-    Then("""I have to pay""") { () =>
+    Then("""^I have to pay$""") {
         p.value == 10
     }
 
