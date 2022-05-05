b8887cd61525b754264094486cc263b6803b13e6
andrea.giulianelli <andrygiulio@gmail.com>
2022-05-05 11:01:21 +0200

    es6 with jacoco commented out

--- src/test/scala/tLab/es6/TestShoppingSteps.scala
+++ src/test/scala/tLab/es6/TestShoppingSteps.scala
@@ -2,18 +2,26 @@ package tLab.es6
 
 import io.cucumber.scala.{EN, ScalaDsl}
 import org.junit.jupiter.api.Assertions
-import testLab.Price
+import testLab.{BasicCart, BasicCatalog, BasicLogger, BasicWarehouse, Price, Product, Shopping}
 
 class TestShoppingSteps extends ScalaDsl with EN:
-    val p = Price(10)
+    val (p1,p2) = (Product("Shoe"), Product("Hat"))
+    val warehouse = new BasicWarehouse
+    val catalog = new BasicCatalog(Map[Product,Price](
+        p1 -> Price(78),
+        p2 -> Price(34)
+    ))
+    val cart = new BasicCart()
+    val shopping = new Shopping(warehouse, catalog, cart, new BasicLogger(">> "))
 
     Given("""^A shopping$""") {
-        println("ciao")
+        warehouse.supply(p1, 2)
+        warehouse.supply(p2, 50)
     }
     When("""^I want to but something$""") {
-        println("come")
+        shopping.pick(p1,1)
     }
     Then("""^I have to pay$""") {
-        p.value == 10
+        Assertions.assertTrue(cart.totalCost > 0)
     }
 
